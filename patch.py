9c45ab8e8e2794df6a396c75371bcb7cc600aabd
Mimics2 <kolesnicenkauoevsej@gmail.com>
2025-11-27 05:08:57 -0500

    Update patch.py

--- patch.py
+++ patch.py
@@ -1,2 +1,8 @@
 import sys
-sys.modules['imghdr'] = type('MockImghdr', (), {'what': lambda x: None})()
+from unittest.mock import MagicMock
+
+# Создаем мок для imghdr
+mock_imghdr = MagicMock()
+mock_imghdr.what = lambda x: None
+
+sys.modules['imghdr'] = mock_imghdr
